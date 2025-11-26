f8b5bc49f7d6e94ad0cd88da41afac72c33f2522
believeme <cain1227@naver.com>
2025-11-27 00:42:34 +0900

    lesson: #7 데드락 실습 테스트코드 작성

--- src/main/java/com/example/locking/item/ItemService.java
+++ src/main/java/com/example/locking/item/ItemService.java
@@ -2,6 +2,7 @@ package com.example.locking.item;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -26,6 +27,23 @@ public class ItemService {
 	public Item findItemById(Long itemId) {
 		// 비관적 락 없이 데이터를 조회합니다.
 		return itemRepository.findById(itemId).orElse(null);
+	}
+
+	// 데드락 임의 발생 실습
+	@Transactional(timeout = 1, isolation = Isolation.SERIALIZABLE)
+	public void updateItemsQuantity(Long itemId1, Long itemId2) {
+		// 첫 번째 아이템에 락을 건 후 업데이트
+		Item item1 = itemRepository.findByIdWithLock(itemId1);
+		item1.setQuantity(item1.getQuantity() + 10);
+		itemRepository.save(item1);
+
+		// 데드락을 발생시키기 위해 스레드 잠시 중지
+		try { Thread.sleep(4000); } catch (InterruptedException e) { e.printStackTrace(); }
+
+		// 두 번째 아이템에 락을 건 후 업데이트
+		Item item2 = itemRepository.findByIdWithLock(itemId2);
+		item2.setQuantity(item2.getQuantity() + 10);
+		itemRepository.save(item2);
 	}
 }
 
